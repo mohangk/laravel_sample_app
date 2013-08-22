64c05ab9e0884257c37c777b8fc11a7ceaff8dd1
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2013-08-22 15:40:54 +0800

    expand home controller test

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,15 +3,13 @@
 class HomeController extends BaseController {
 
   public function index() {
-
     $users = App::make('User')->all();
     $uniqueVisitorsByDate = App::make('Metric')->uniqueVisitors();
     $pageviewsByDate = App::make('Metric')->pageViews();
 
-    return View::make('home', ['users' => $users,
+    return View::make('home',[ 'users' => $users,
                                'uniqueVisitorsByDate' => $uniqueVisitorsByDate,
-                               'pageviewsByDate' => $pageviewsByDate
-                              ]);
+                               'pageviewsByDate' => $pageviewsByDate ]);
 
 	}
 
--- tests/unit/controllers/HomeControllerTest.php
+++ tests/unit/controllers/HomeControllerTest.php
@@ -4,28 +4,39 @@ class HomeControllerTest extends ControllerTest {
 
   use Codeception\Specify;
 
-  protected $user;
-
-  public function setUp() {
-    parent::setUp();
-    $this->user = new User(['name' => 'Sean Carter',
-                            'email' => 'jay-z@example.com',
-                            'password' => 'password',
-                            'created_at' => new DateTime(),
-                            'updated_at' => new DateTime()]);
-  }
-
   public function testIndex() {
     $this->path = URL::action('HomeController@index', [], false);
 
     $this->specify("it renders all users", function() {
-      App::instance('User', Mockery::mock('User', ['all' => [$this->user]]));
+      $user = new User(['name' => 'Sean Carter',
+                        'email' => 'jay-z@example.com',
+                        'password' => 'password',
+                        'created_at' => new DateTime(),
+                        'updated_at' => new DateTime()]);
+
+      App::instance('User', Mockery::mock('User', ['all' => [$user]]));
 
       $response = $this->get($this->path);
       $this->assertResponseOk();
       $this->assertContains('Sean Carter', $response->getContent());
     });
 
+    $this->specify("it renders all metrics", function() {
+      $metricMock = Mockery::mock('Metric');
+
+      $uniqueVisitor = new Metric(['date' => new DateTime(), 'count' => 31337 ]);
+      $metricMock->shouldReceive('uniqueVisitors')->andReturn([$uniqueVisitor]);
+
+      $pageView = new Metric(['date' => new DateTime(), 'count' => 1234567 ]);
+      $metricMock->shouldReceive('pageViews')->andReturn([$pageView]);
+
+      App::instance('Metric', $metricMock);
+
+      $response = $this->get($this->path);
+      $this->assertResponseOk();
+      $this->assertContains(strval($uniqueVisitor->count), $response->getContent());
+      $this->assertContains(strval($pageView->count), $response->getContent());
+    });
   }
 
 }
