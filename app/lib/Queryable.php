8313d144f9c7c63077514597106b1bdcabb9106f
Mohan Krishnan <mohangk@gmail.com>
2013-08-23 15:30:14 +0800

    extract a queryable trait

---
+++ app/lib/Queryable.php
@@ -0,0 +1,18 @@
+<?php
+
+trait Queryable {
+
+  public static function findOrInitializeBy($whereArray) {
+    $query = (new static)->newQuery();
+    foreach($whereArray as $column => $value) {
+      $query = $query->where($column, '=', $value);
+    }
+
+    if($metric = $query->first()) {
+      return $metric;
+    } else {
+      return new static($whereArray);
+    }
+  }
+
+}
--- app/models/Metric.php
+++ app/models/Metric.php
@@ -4,6 +4,7 @@ use \Carbon\Carbon;
 
 class Metric extends Eloquent {
   use Validatable;
+  use Queryable;
 
   const PAGEVIEWS = 'ga:pageviews';
   const UNIQUE_VISITORS = 'ga:visitors';
@@ -21,19 +22,6 @@ class Metric extends Eloquent {
 
   public function getDates() {
     return ['date'];
-  }
-
-  public static function findOrInitializeBy($whereArray) {
-    $query = (new static)->newQuery();
-    foreach($whereArray as $column => $value) {
-      $query = $query->where($column, '=', $value);
-    }
-
-    if($metric = $query->first()) {
-      return $metric;
-    } else {
-      return new static($whereArray);
-    }
   }
 
   public static function uniqueVisitors() {
